a6beb68b6801a03458b6c0df372358d93bc6daf9
Keren <124227765+KerenWL@users.noreply.github.com>
2023-05-04 20:48:59 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name        NeoLMS+ Userscript
+// @namespace   Violentmonkey Scripts
+// @include     /.*lms\..*|.*\/.*lms.*/
+// @grant       none
+// @version     1.0
+// @author      K8y
+// ==/UserScript==
+
+window.addEventListener('load', (e) => {
+	// Display readings inside page.
+	const handout_readings_viewer = document.querySelectorAll(
+		'div.materialStyle > p > a[href*="files"][target="_blank"][rel="noopener"]'
+	);
+	if (handout_readings_viewer.length) {
+		for (const el of handout_readings_viewer) {
+			const iframe = document.createElement('iframe');
+			iframe.src = el.getAttribute('href');
+			iframe.style.maxWidth = '100%';
+			iframe.style.width = '810px';
+			iframe.style.height = '698px';
+			iframe.setAttribute('loading', 'lazy');
+			el.parentNode.insertBefore(iframe, el);
+
+			// Add br element for better formatting.
+			const br = document.createElement('br');
+			el.parentNode.insertBefore(br, el);
+
+			el.parentNode.style.textAlign = 'center';
+		}
+	}
+
+	// Convert show_event onCLicks to proper hrefs
+	const onEventLinks = document.querySelectorAll('a[onClick*="show_event"]');
+	if (onEventLinks.length) {
+		for (const el of onEventLinks) {
+			const matches = Array.from(el.getAttribute('onClick').matchAll(/show_event\('(.+)'\)/g), (m) => m[1]);
+			if (matches.length) {
+				const match = matches[0];
+
+				el.href = match;
+				el.removeAttribute('onClick');
+			}
+		}
+	}
+});
